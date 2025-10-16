878b7d795f62860712eb113b9ca1b244d36acf59
timsun <t_nurlanov@kbtu.kz>
2025-10-16 23:31:02 +0500

    project(hw4)

--- project/my-app/src/components/UserList.jsx
+++ project/my-app/src/components/UserList.jsx
@@ -6,39 +6,58 @@ export default function UserList() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [search, setSearch] = useState("");
 
 async function loadUsers() {
-    try {
-      setLoading(true);
-      setError("");
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
-      if (!res.ok) throw new Error("Failed to fetch users");
-      const data = await res.json();
-      setUsers(data);
-    } catch (e) {
-      setError(e.message || "Unknown error");
-    } finally {
-      setLoading(false);
-    }
+  try {
+    setLoading(true);
+    setError("");
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    if (!res.ok) throw new Error("Failed to fetch users");
+    const data = await res.json();
+    setUsers(data);
+  } catch (e) {
+    setError(e.message || "Unknown error");
+  } finally {
+    setLoading(false);
+  }
 }
 
+const filteredUsers = users.filter((u) =>
+  u.name.toLowerCase().includes(search.toLowerCase())
+);
+
 return (
-    <section className="userlist">
-      <h1 className="ul-title">Users</h1>
+  <section className="userlist">
+    <h1 className="ul-title">Users</h1>
 
-      <button className="ul-btn" onClick={loadUsers} disabled={loading}>
-        {loading ? "Loading..." : users.length ? "Reload" : "Load"}
-      </button>
+    <button className="ul-btn" onClick={loadUsers} disabled={loading}>
+      {loading ? "Loading..." : users.length ? "Reload" : "Load"}
+    </button>
 
-      {error && <p className="ul-error">⚠ {error}</p>}
+    <div className="ul-search">
+      <input
+      type="text"
+      placeholder="Search by name.."
+      value={search}
+      onChange={(e) => setSearch(e.target.value)} 
+      />
+      {search && (
+        <button className="clear-btn" onClick={() => setSearch("")}>
+          Clear
+        </button>
+      )}
+    </div>
 
-      <ul className="ul-grid">
-        {users.map((u) => (
-          <li key={u.id}>
-            <UserCard user={u} />
-          </li>
-        ))}
-      </ul>
-    </section>
+    {error && <p className="ul-error">⚠ {error}</p>}
+
+    <ul className="ul-grid">
+      {filteredUsers.map((u) => (
+        <li key={u.id}>
+          <UserCard user={u} />
+        </li>
+      ))}
+    </ul>
+  </section>
 );
 }
